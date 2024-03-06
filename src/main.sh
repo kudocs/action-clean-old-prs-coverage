55ce0fbf40813d562fa59d7b780375ff0bc01776
Guilherme Pereira <guiferrpereira@gmail.com>
2024-03-06 20:43:32 +0000

    Fixup code coverage failed job when nothing to commit

--- src/main.sh
+++ src/main.sh
@@ -1,28 +1,32 @@
 main(){
+  export GITHUB_TOKEN="$1"
 
-export GITHUB_TOKEN="$1"
+  GITHUB_API_HEADER="Accept: application/vnd.github.v3+json"
+  GITHUB_API_URI="https://api.github.com"
 
-GITHUB_API_HEADER="Accept: application/vnd.github.v3+json"
-GITHUB_API_URI="https://api.github.com"
+  repos=($(ls -d -- */))
 
-repos=($(ls -d -- */))
+  for repo in "${repos[@]}"; do
+    repository="${repo%?}"
 
-for repo in "${repos[@]}"; do
-  repository="${repo%?}"
+    echo "Cleaning $repository old files"
 
-  echo "Cleaning $repository old files"
+    open_prs=($(curl -sSL -H "$GITHUB_API_HEADER" -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API_URI/repos/kudocs/$repository/pulls"| jq '.[].number'))
 
-  open_prs=($(curl -sSL -H "$GITHUB_API_HEADER" -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API_URI/repos/kudocs/$repository/pulls"| jq '.[].number'))
+    array=("${open_prs[@]/#/ -not -name }")
+    command=$(printf '%s' "${array[@]}")
 
-  array=("${open_prs[@]/#/ -not -name }")
-  command=$(printf '%s' "${array[@]}")
+    find ./$repository -mindepth 1 -maxdepth 1 -type d -not -name dev -not -name master -not -name live ${command} -exec rm -rf '{}' \;
+  done
 
-  find ./$repository -mindepth 1 -maxdepth 1 -type d -not -name dev -not -name master -not -name live ${command} -exec rm -rf '{}' \;
-done
+  modifiedFiles=($(git status | grep 'modified:' | wc -l))
 
-git add . ;
-git -c user.name="GitHub Actions" -c user.email="actions@github.com" \
-        commit -m "Delete old coverage files" ;
-git push;
-
+  if [ "$modifiedFiles" -gt 0 ]; then
+    git add . ;
+    git -c user.name="GitHub Actions" -c user.email="actions@github.com" \
+            commit -m "Delete old coverage files" ;
+    git push;
+  else
+    echo "Nothing changed"
+  fi
 }
